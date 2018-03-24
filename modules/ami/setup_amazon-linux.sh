feaf508d6d551d485673f184658e9c5b9dc37bef
Matthias Scholz <matthias.scholz@gmail.com>
2018-03-24 15:37:29 +0100

    .NEW Activate privileged mode for nomad client nodes - DANGEROUS!
    Some services might need to run Docker container in privileged
    mode. For example the buidsystem Concourse. To facilitate the handling
    for this demo setup one AMI with activated privileged mode is
    generate.
    Nevertheless activating the privileged mode in general for all client
    nodes is dangerous. For the future the terraform module has to be
    updated to use different AMIs for different datacenters,
    e.g. activating privileged mode support for backoffice and deactivate
    it for public-services.

--- modules/ami/setup_amazon-linux.sh
+++ modules/ami/setup_amazon-linux.sh
@@ -7,6 +7,9 @@ echo "[INFO] [${SCRIPT}] Setup git"
 sudo yum install -y git
 
 echo "[INFO] [${SCRIPT}] Setup docker"
-sudo yum install -y docker
+# There is a bug in docker version: 17.12.0ce-1.129.amzn1
+# Regarding privileged container handling:
+# .https://github.com/docker/for-linux/issues/219
+sudo yum install -y docker-17.09.1ce-1.111.amzn1.x86_64
 sudo service docker start
 sudo usermod -a -G docker ec2-user
--- modules/ami/setup_nomad_consul.sh
+++ modules/ami/setup_nomad_consul.sh
@@ -2,8 +2,10 @@
 set -e
 
 # Environment variables are set by packer
+# NOTE: Script will try to update all installed packages
 /tmp/install-nomad/install-nomad --version "${NOMAD_VERSION}"
 
 # FIXME: This is so ugly! Why not catching the binaries directly?
+# NOTE: Consul will try to update all installed packages
 git clone --branch "${CONSUL_MODULE_VERSION}"  https://github.com/hashicorp/terraform-aws-consul.git /tmp/terraform-aws-consul
 /tmp/terraform-aws-consul/modules/install-consul/install-consul --version "${CONSUL_VERSION}"
