1e81144e26f5be3f386b4a3bad8ad3a723574b28
Matthias Scholz <matthias.scholz@gmail.com>
2018-03-26 09:49:09 +0200

    .NEW simple test script with job runs
    Tests:
    . nomad system job
    . nomad service job with ALB connection
    . nomad service job group with privileged

---
+++ examples/root-example/run_tests.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -e
+
+SCRIPT=`basename "$0"`
+
+echo "[INFO] [${SCRIPT}] Testing the Nomad Cluster"
+server_ip=$(get_nomad_server_ip.sh) &&          \
+    export NOMAD_ADDR=http://$server_ip:4646 && \
+    echo ${NOMAD_ADDR}
+
+echo "[INFO] [SCRIPT] Current state jobs"
+nomad status
+echo "[INFO] [SCRIPT] Current state server"
+nomad server-members
+echo "[INFO] [SCRIPT] Current state nodes"
+nomad node-status
+
+echo "[INFO] [SCRIPT] Deploying some jobs"
+nomad run ../jobs/fabio.nomad
+nomad run ../jobs/ping_service.nomad
+nomad run ../jobs/cicd.nomad
+
+sleep 5
+
+echo "[INFO] [SCRIPT] Current state jobs"
+nomad status
