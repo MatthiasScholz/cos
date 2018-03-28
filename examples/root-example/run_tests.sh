0b7767e310c35a1de515bce5d04644e44e9470f5
Thomas Obenaus <thomas.obenaus@preh.de>
2018-03-28 23:45:21 +0200

    .FIX fixed typo in run-test script

--- examples/root-example/run_tests.sh
+++ examples/root-example/run_tests.sh
@@ -8,19 +8,19 @@ server_ip=$(get_nomad_server_ip.sh) &&          \
     export NOMAD_ADDR=http://$server_ip:4646 && \
     echo ${NOMAD_ADDR}
 
-echo "[INFO] [SCRIPT] Current state jobs"
+echo "[INFO] [${SCRIPT}] Current state jobs"
 nomad status
-echo "[INFO] [SCRIPT] Current state server"
+echo "[INFO] [${SCRIPT}] Current state server"
 nomad server-members
-echo "[INFO] [SCRIPT] Current state nodes"
+echo "[INFO] [${SCRIPT}] Current state nodes"
 nomad node-status
 
-echo "[INFO] [SCRIPT] Deploying some jobs"
+echo "[INFO] [${SCRIPT}] Deploying some jobs"
 nomad run ../jobs/fabio.nomad
 nomad run ../jobs/ping_service.nomad
 nomad run ../jobs/cicd.nomad
 
 sleep 5
 
-echo "[INFO] [SCRIPT] Current state jobs"
+echo "[INFO] [${SCRIPT}] Current state jobs"
 nomad status
