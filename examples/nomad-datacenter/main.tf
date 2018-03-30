6732d427fd767da26aeed4d052d26d3b5666d189
Thomas Obenaus <thomas.obenaus@preh.de>
2018-03-30 11:54:14 +0200

    .ADD example and module nomad-datacenter

---
+++ examples/nomad-datacenter/main.tf
@@ -0,0 +1,37 @@
+locals {
+  aws_region = "us-east-1"
+  stack_name = "COS"
+  env_name   = "playground"
+}
+
+provider "aws" {
+  profile = "${var.deploy_profile}"
+  region  = "${local.aws_region}"
+}
+
+### obtaining default vpc, security group and subnet of the env
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "all" {
+  vpc_id = "${data.aws_vpc.default.id}"
+}
+
+module "nomad_datacenter" {
+  source = "../../modules/nomad_datacenter"
+
+  ## required parameters
+  vpc_id     = "${data.aws_vpc.default.id}"
+  subnet_ids = "${data.aws_subnet_ids.all.ids}"
+  ami_id     = "ami-a23feadf"
+
+  ## optional parameters
+  aws_region              = "${local.aws_region}"
+  env_name                = "${local.env_name}"
+  stack_name              = "${local.stack_name}"
+  cluster_tag_key         = "consul-servers"
+  cluster_tag_value       = "${local.stack_name}-${local.env_name}-consul-srv"
+  allowed_ssh_cidr_blocks = ["0.0.0.0/0"]
+  ssh_key_name            = "kp-us-east-1-playground-instancekey"
+}
---
+++ modules/nomad-datacenter/ecr.tf
@@ -0,0 +1,35 @@
+# Add policy to the already created iam role of the nomad clients in the nomad cluster module.
+# Policy-attachment that grants read access to AWS ECR for nomad clients
+resource "aws_iam_role_policy_attachment" "irpa_ecr_read_access" {
+  # FIXME: Because of this constellation it is not possible to provide the ECR access configuration as module.
+  role       = "${module.clients_public_services.iam_role_id}"
+  policy_arn = "${aws_iam_policy.ip_ecr_read_access.arn}"
+}
+
+resource "aws_iam_role_policy_attachment" "irpa_ecr_read_access_private_services" {
+  role       = "${module.clients_private_services.iam_role_id}"
+  policy_arn = "${aws_iam_policy.ip_ecr_read_access.arn}"
+}
+
+resource "aws_iam_role_policy_attachment" "irpa_ecr_read_access_content_connector" {
+  role       = "${module.clients_content_connector.iam_role_id}"
+  policy_arn = "${aws_iam_policy.ip_ecr_read_access.arn}"
+}
+
+resource "aws_iam_policy" "ip_ecr_read_access" {
+  name   = "APP-${var.stack_name}-${var.aws_region}-${var.env_name}-IP-ECR-read-${var.unique_postfix}"
+  policy = "${data.aws_iam_policy_document.ipd_ecr_read_access.json}"
+}
+
+data "aws_iam_policy_document" "ipd_ecr_read_access" {
+  statement {
+    actions = [
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+    ]
+
+    resources = ["*"]
+  }
+}
