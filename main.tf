aa4d5d5a47f860ebe45e49d8d213ffa5a316aa39
Thomas Obenaus <thomas.obenaus@preh.de>
2018-03-30 13:34:05 +0200

    .UPDATE instantiation of nomad-datacenter module

--- main.tf
+++ main.tf
@@ -36,6 +36,28 @@ module "consul" {
   ssh_key_name            = "${var.ssh_key_name}"
 }
 
+module "dc-public-services" {
+  source = "modules/nomad-datacenter"
+
+  ## required parameters
+  vpc_id                   = "${var.vpc_id}"
+  subnet_ids               = "${var.nomad_clients_public_services_subnet_ids}"
+  ami_id                   = "${var.nomad_ami_id_clients}"
+  consul_cluster_tag_key   = "${local.consul_cluster_tag_key}"
+  consul_cluster_tag_value = "${local.consul_cluster_tag_value}"
+  server_sg_id             = "${module.nomad.security_group_id_nomad_servers}"
+
+  ## optional parameters
+  env_name                = "${var.env_name}"
+  stack_name              = "${var.stack_name}"
+  aws_region              = "${var.aws_region}"
+  instance_type           = "${var.instance_type_client}"
+  allowed_ssh_cidr_blocks = "${var.allowed_ssh_cidr_blocks}"
+  ssh_key_name            = "${var.ssh_key_name}"
+  datacenter_name         = "public-services"
+  unique_postfix          = "${var.unique_postfix}"
+}
+
 module "nomad" {
   source                               = "modules/nomad"
   env_name                             = "${var.env_name}"
