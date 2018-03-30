64adb41a48e2aa14ce25ea3a6d5dd3b2cfdd26e4
Matthias Scholz <matthias.scholz@gmail.com>
2018-03-30 17:31:36 +0200

    .NEW activate Consul Client DNS Forwarding

--- modules/ami2/setup_nomad_consul.sh
+++ modules/ami2/setup_nomad_consul.sh
@@ -9,3 +9,9 @@ set -e
 # FIXME: Removed supervisord usage from consul as well
 git clone --branch "${CONSUL_MODULE_VERSION}"  https://github.com/hashicorp/terraform-aws-consul.git /tmp/terraform-aws-consul
 /tmp/terraform-aws-consul/modules/install-consul/install-consul --version "${CONSUL_VERSION}"
+
+# Activate Consul DNS Forwarding
+# .The consul configuration file was injected in the packer configuration before.
+sudo yum install -y dnsmasq
+sudo systemctl enable dnsmasq
+sudo mv /tmp/dnsmasq_10-consul.conf /etc/dnsmasq.d/10-consul
