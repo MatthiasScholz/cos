6291e4ed70e7187e67440a11b4f0a8e81944b856
Thomas Obenaus <thomas.obenaus@preh.de>
2018-03-30 13:28:06 +0200

    .UPADTE updated outputs

--- modules/nomad-datacenter/outputs.tf
+++ modules/nomad-datacenter/outputs.tf
@@ -1,49 +1,15 @@
-#output "num_nomad_servers" {
-#  value = "${module.nomad_servers.cluster_size}"
-#}
-#
-#output "asg_name_nomad_servers" {
-#  value = "${module.nomad_servers.asg_name}"
-#}
-#
-#output "launch_config_name_nomad_servers" {
-#  value = "${module.nomad_servers.launch_config_name}"
-#}
-#
-#output "iam_role_arn_nomad_servers" {
-#  value = "${module.nomad_servers.iam_role_arn}"
-#}
-#
-#output "iam_role_id_nomad_servers" {
-#  value = "${module.nomad_servers.iam_role_id}"
-#}
-#
-#output "security_group_id_nomad_servers" {
-#  value = "${module.nomad_servers.security_group_id}"
-#}
-#
-#output "num_clients_public_services" {
-#  value = "${module.clients_public_services.cluster_size}"
-#}
-#
-#output "asg_name_clients_public_services" {
-#  value = "${module.clients_public_services.asg_name}"
-#}
-#
-#output "aws_region" {
-#  value = "${var.aws_region}"
-#}
-#
-#output "nomad_servers_cluster_tag_key" {
-#  value = "${module.nomad_servers.cluster_tag_key}"
-#}
-#
-#output "nomad_servers_cluster_tag_value" {
-#  value = "${module.nomad_servers.cluster_tag_value}"
-#}
-#
-#output "clients_public_services_cluster_tag_value" {
-#  value = "${module.clients_public_services.cluster_tag_value}"
-#}
-#
+output "num_nodes" {
+  value = "${module.data_center.cluster_size}"
+}
 
+output "asg_name" {
+  value = "${module.data_center.asg_name}"
+}
+
+output "aws_region" {
+  value = "${var.aws_region}"
+}
+
+output "cluster_tag_value" {
+  value = "${module.data_center.cluster_tag_value}"
+}
