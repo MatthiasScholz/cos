4276121ffcabb1123f7f53895b32b340067cfbb0
Thomas Obenaus <thomas.obenaus@preh.de>
2018-04-08 15:21:09 +0200

    .UPDATE adjust name for nomad clients

---
+++ modules/nomad-datacenter/main.tf
@@ -0,0 +1,10 @@
+# Terraform 0.9.5 suffered from https://github.com/hashicorp/terraform/issues/14399, which causes this template the
+# conditionals in this template to fail.
+terraform {
+  required_version = ">= 0.9.3, != 0.9.5"
+}
+
+locals {
+  short_dc_name     = "${format("%.10s",var.datacenter_name)}"
+  base_cluster_name = "${var.stack_name}-NMC-${local.short_dc_name}"
+}
