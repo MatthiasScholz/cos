85fad67c060ae26b3b290f308d465e935055472e
Thomas Obenaus <thomas.obenaus@preh.de>
2018-04-21 00:04:53 +0200

    .FIX restricted source_ami_filter to "name": "*amzn2-ami-hvm-*gp2" in order to avoid using newest version of other images like the WebOS ones.

--- modules/ami2/setup_nomad_consul.sh
+++ modules/ami2/setup_nomad_consul.sh
@@ -2,11 +2,13 @@
 set -e
 
 # Environment variables are set by packer
+echo "[INFO] [${SCRIPT}] Setup nomad ${NOMAD_VERSION}"
 /tmp/install-nomad/install-nomad --version "${NOMAD_VERSION}"
 
 # FIXME: Removed supervisord usage from consul as well
 # FIXME: This is so ugly! Why not catching the binaries directly?
 # FIXME: Removed supervisord usage from consul as well
+echo "[INFO] [${SCRIPT}] Setup consul ${CONSUL_VERSION}"
 git clone --branch "${CONSUL_MODULE_VERSION}"  https://github.com/hashicorp/terraform-aws-consul.git /tmp/terraform-aws-consul
 /tmp/terraform-aws-consul/modules/install-consul/install-consul --version "${CONSUL_VERSION}"
 
