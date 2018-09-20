87c3035a89c2233183ffc8ccf467abd1c76803a1
Thomas Obenaus <obenaus.thomas@gmail.com>
2018-09-20 20:55:39 +0200

    .Update first version of script for mounting ebs volumes

--- modules/nomad-datacenter/user-data-nomad-client.sh
+++ modules/nomad-datacenter/user-data-nomad-client.sh
@@ -28,3 +28,68 @@ if [ -n "$EFS_DNS_NAME" ];then
 else
   echo "Don't mount efs on this machine, since no efs target is given (env-var EFS_DNS_NAME is not set)."
 fi
+
+
+# space separated list of device names
+# TODO inject tf var
+device_names=("/dev/xvde:/mnt/map1" "/dev/xvdf:/mnt/map2")
+
+# TODO inject tf var
+fs_type="xfs"
+declare -A device_map
+
+
+function parse_device_map () {
+  for device_kv in ${device_names[*]}
+  do
+
+    echo $device_kv
+    while IFS=':' read device mount_target; do
+        echo "$device --> $mount_target"
+        device_map["${device}"]="${mount_target}"
+    done <<<"$device_kv"
+  done
+}
+
+function print_device_map () {
+  for device in ${!device_map[@]}
+  do
+    echo "$device --> ${device_map[${device}]}"
+  done
+}
+
+function prepare_and_mount_device () {
+
+  for device in ${!device_map[@]}
+  do
+    mount_target="${device_map[${device}]}"
+    echo "Create mount_target $mount_target"
+    sudo mkdir -p $mount_target
+
+    mount_command="sudo mount $device $mount_target"
+    formatted="$mount_command 2> /dev/null"
+
+    echo "Create file-system on $device if needed"
+    if eval $formatted; then
+      echo "File system exists"
+    else
+      echo "File system does not exist ... will be created."
+      sudo mkfs -t $fs_type $device -V -V
+    fi
+
+    if grep -q $mount_target /etc/fstab
+    then
+      echo "mountpoint already in /etc/fstab"
+    else
+      echo "Update /etc/fstab $device -> $mount_target"
+      sudo echo -e "$device $mount_target $fs_type defaults 0 0" | sudo tee --append /etc/fstab
+    fi
+
+    echo "Mounting $device on $mount_target"
+    sudo mount -a
+  done
+}
+
+parse_device_map
+print_device_map
+prepare_and_mount_device
