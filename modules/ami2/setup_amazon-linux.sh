f2e2799e7efa0ef9519d17314c600854ccec27b9
Matthias Scholz <matthias.scholz@gmail.com>
2018-10-07 18:15:15 +0200

    .NEW added time synchronization support using preconfigured chrony

--- modules/ami2/setup_amazon-linux.sh
+++ modules/ami2/setup_amazon-linux.sh
@@ -11,5 +11,10 @@ sudo yum install -y docker
 sudo systemctl enable docker
 sudo usermod -a -G docker ec2-user
 
+echo "[INFO] [${SCRIPT}] Setup time synchronisation"
+sudo systemctl enable chronyd
+sudo systemctl status chronyd
+
 echo "[INFO] [${SCRIPT}] Cleanup packages"
 sudo yum remove -y powershell
+
