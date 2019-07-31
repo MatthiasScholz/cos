00223f74e1e458f41b380d9253759ea7253a41e2
Matthias Scholz <matthias.scholz@gmail.com>
2019-07-31 13:49:23 +0200

    .BUGFIX broken terratest for bastion example
    Test was broken due to streamlined infrastructure setup.
    The example does not require an "aws_region" to be set.
    Nevertheless to have generic terraform modules this should be the case.
    The example should be updated to support different regions.

--- examples/bastion/vars.tf
+++ examples/bastion/vars.tf
@@ -6,3 +6,8 @@ variable "ssh_key_name" {
   description = "Name of the SSH instance key to be used."
   default = "kp-us-east-1-playground-instancekey"
 }
+
+variable "aws_region" {
+  description = "Dummy to make use of generic terraform configuration in tests."
+  default = ""
+}
