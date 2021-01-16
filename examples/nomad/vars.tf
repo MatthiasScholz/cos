73ae068d9ad202c61182a32aeca4c2df79b81403
Matthias Scholz <matthias.scholz@thoughtworks.com>
2021-01-16 13:17:59 +0100

    :recycle: making AMI dependency explicit

--- examples/nomad/vars.tf
+++ examples/nomad/vars.tf
@@ -4,9 +4,6 @@ variable "deploy_profile" {
 
 variable "ami_id" {
   description = "Id of the AMI for the nomad and consul nodes."
-  default     = "ami-09118e4b58586b75d"
-}
-
 }
 
 variable "aws_region" {
